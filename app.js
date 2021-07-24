2561ff96f803bd1a277627a0bc7afb642b0eae13
myaryhin0201 <yarygin45@gmail.com>
2021-07-24 16:41:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-const authRouter = require("./routes/auth");
-const { errorHandler } = require("./helpers/apiHelpers");
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const { errorHandler } = require('./helpers/apiHelpers');
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json({ limit: 1000 }));
 
-app.use("/users", authRouter);
+app.use('/users', authRouter);
+app.use('/products', productRouter);
 
 // пока закомментил (Витя)
 // app.use(express.static(process.env.PUBLIC_DIR))
 
 // Ответ на всех урлы, которые не заматчились
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 // app.use() // Обработчик всех ошибок - добавить
