8cf504672b82c9be3ca76dc5d4ac7f0b15b85c7f
myaryhin0201 <yarygin45@gmail.com>
2021-07-25 13:02:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const authRouter = require('./routes/auth');
 const productRouter = require('./routes/product');
 const userRouter = require('./routes/user');
+const dayRouter = require('./routes/day');
 const { errorHandler } = require('./helpers/apiHelpers');
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -16,9 +17,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json({ limit: 1000 }));
 
-app.use('/users', authRouter);
+app.use('/auth', authRouter);
 app.use('/user', userRouter);
 app.use('/products', productRouter);
+app.use('/day', dayRouter);
 
 // Ответ на всех урлы, которые не заматчились
 app.use((req, res) => {
