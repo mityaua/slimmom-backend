fcf5dcaa6d04685e21a32b7eb06de3d9fc4d25be
myaryhin0201 <yarygin45@gmail.com>
2021-07-25 10:15:46 +0200

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,9 +1,27 @@
 const { User } = require('../models/userModel');
 
-const getUserById = async id => {
-  return await User.findById(id);
+const getUserById = async userId => {
+  return await User.findById(userId);
+};
+// day
+const findUserByIdAndUpdateDays = async (userId, day) => {
+  const { _id, date } = day;
+  return await User.findByIdAndUpdate(userId, {
+    $push: { days: { id: id, date } },
+  });
+};
+
+// daily-rate
+const findUserByIdAndUpdateUserData = async (userId, reqBody, dailyRate) => {
+  return await User.findByIdAndUpdate(
+    userId,
+    { userData: { ...reqBody, dailyRate } },
+    { new: true },
+  );
 };
 
 module.exports = {
   getUserById,
+  findUserByIdAndUpdateDays,
+  findUserByIdAndUpdateUserData,
 };
