6af5a109130316197582f1eab1d0977b6d367c62
yarymaks <yarygin45@gmail.com>
2021-08-01 20:18:09 +0200

    fix search

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,8 +3,8 @@ const { NotFoundError } = require('../helpers/errors');
 
 const getProduct = async (req, res) => {
   try {
-    const search = decodeURI(req.query.search);
-    const product = await Product.getProducts(search);
+    // const search = decodeURI(req.query.search);
+    const product = await Product.getProducts(req.query.search);
     return res.status(200).json(product);
   } catch (error) {
     throw new NotFoundError('Product not found');
--- services/productService.js
+++ services/productService.js
@@ -2,7 +2,7 @@ const Product = require('../models/productModel');
 
 const getProducts = async query => {
   const products = await Product.find({
-    'title.ru': { $regex: `^${query}`, $options: '$i' },
+    'title.ru': { $regex: `${query}`, $options: 'i' },
   });
   return products;
 };
