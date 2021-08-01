d75606481c8ef8140db855b3daf9fd11d2265da4
yarymaks <yarygin45@gmail.com>
2021-08-01 19:15:51 +0200

    Update calculateEatenProduct.js

--- helpers/dayHelpers/calculateEatenProduct.js
+++ helpers/dayHelpers/calculateEatenProduct.js
@@ -1,6 +1,6 @@
 const calculateEatenProduct = (eatenProduct, weight) => {
   const { title, calories } = eatenProduct;
-  const kcal = (calories * weight) / eatenProduct.weight;
+  const kcal = Math.floor((calories * weight) / eatenProduct.weight);
   return { title: title.ru, weight, kcal };
 };
 
