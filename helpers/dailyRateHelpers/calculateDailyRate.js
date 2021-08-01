8b5e9857193bae168ef987b72e48554afba0e0ab
yarymaks <yarygin45@gmail.com>
2021-08-01 18:34:12 +0200

    Update calculateDailyRate.js

--- helpers/dailyRateHelpers/calculateDailyRate.js
+++ helpers/dailyRateHelpers/calculateDailyRate.js
@@ -1,10 +1,10 @@
 const calculateDailyRate = ({ currentWeight, height, age, desiredWeight }) => {
-  return (
+  return Math.floor(
     10 * currentWeight +
-    6.25 * height -
-    5 * age -
-    161 -
-    10 * (currentWeight - desiredWeight)
+      6.25 * height -
+      5 * age -
+      161 -
+      10 * (currentWeight - desiredWeight),
   );
 };
 
